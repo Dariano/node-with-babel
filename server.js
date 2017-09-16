029988d474e42a747f6c2199d15f4c02e818b95a
dariano <darianosoares@hotmail.com>
2017-09-16 00:28:37 -0300

    Adicionado middleware na rota

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ import bodyParser from 'body-parser'
 const app = express()
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Olá pessoal!!!!'))
+const logMiddleware = (req, res, next) => {
+    console.log('Passei por aqui!!')
+
+    next()
+}
+
+app.get('/', logMiddleware, (req, res) => res.send('Olá pessoal!!!!'))
 
 app.listen(3000, () => console.log('Rodando na porta 3000!'))
